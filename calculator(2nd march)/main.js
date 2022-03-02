d500b5deea9704a13ae84b8e46c61fe597596a96
rasheed8123 <64723816+rasheed8123@users.noreply.github.com>
2022-03-02 18:37:22 +0530

    Add files via upload

---
+++ calculator(2nd march)/main.js
@@ -0,0 +1,12 @@
+// addition
+const add = require("./add")
+add(10,20)
+// substraction
+const sub = require("./subtract")
+sub(10,20)
+// multiplication
+const mul = require("./multiply")
+mul(10,20)
+// divivsion
+const div = require("./divide")
+div(10,20)
---
+++ calculator(2nd march)/subtract.js
@@ -0,0 +1,3 @@
+module.exports = function(a,b){
+    console.log("SUBSTRACTION = " , a-b)
+}
